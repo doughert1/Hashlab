3d9a51b50a08d9db0621dd4c461aadabdb70ff3a
Alex Greenberg <greenber@grinnell.edu>
2014-04-17 12:00:17 -0500

    Hashlab

---
+++ hashtables/src/taojava/util/Reporter.java
@@ -0,0 +1,12 @@
+package taojava.util;
+
+/**
+ * A simple mechanism to allow programs to report steps in different ways.
+ */
+public interface Reporter
+{
+  /**
+   * Report.
+   */
+  public void report(String str);
+} // Reporter
